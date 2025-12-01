a87b965da5dcfab27e1830256a42228b88d8b5c0
Natouille04 <nathan.gouveia2008@gmail.com>
2025-12-01 11:51:11 +0100

    Initial commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+const SponsRes = document.querySelector("#taw");
+const BtmSponsRes = document.querySelector("#bottomads");
+
+console.log("ok on est sur google, c'est l'heure de terminatorisée tout ce bordel");
+
+function remSpons(div) {
+    div.remove();
+}
+
+remSpons(SponsRes);
+remSpons(BtmSponsRes);
